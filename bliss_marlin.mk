dd9e506f764aa1987bbf72a62cb8d819b02bd50f
Waseem Alkurdi <waseem19992009@outlook.com>
2020-05-28 01:25:22 +0300

    Maintainer name tag, always build as official

--- bliss_marlin.mk
+++ bliss_marlin.mk
@@ -25,3 +25,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/marlin/marlin:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
+
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.bliss.maintainer=Waseem Alkurdi
+
+BLISS_BUILDTYPE=OFFICIAL
